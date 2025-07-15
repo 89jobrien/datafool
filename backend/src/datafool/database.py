ff9d42506ac28ec8013c74a4c2fe932423b4158f
89jobrien <98370624+89jobrien@users.noreply.github.com>
2025-07-14 22:54:37 -0400

    Update database.py

--- backend/src/datafool/database.py
+++ backend/src/datafool/database.py
@@ -1,23 +1,17 @@
-# backend/database.py
-
 import os
 
 from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
-# Load environment variables from .env file
 load_dotenv()
 
-# Get the database URL from the environment variables
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
     raise ValueError("No DATABASE_URL found in environment variables")
 
-# Create the SQLAlchemy engine for asynchronous interaction
 engine = create_async_engine(DATABASE_URL)
 
-# Create a configured "Session" class for creating new sessions
 AsyncSessionLocal = async_sessionmaker(
     bind=engine,
     class_=AsyncSession,
